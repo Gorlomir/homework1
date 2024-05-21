2e6766b7ec48249b886fb37668e1409ecf67b501
Gorlomir <151527759+Gorlomir@users.noreply.github.com>
2024-05-21 20:47:41 +0500

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+count_homework = 12
+hours = 1.5
+name = 'Python'
+time = ( hours / count_homework)
+print('Курс:',name,', ','всего задач:',count_homework, ', ', 'затрачено часов:', hours, ',', 'среднее время выполнения ', time, 'часа.')
