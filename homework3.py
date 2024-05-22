0047c8ce15e24f72123933502ea074b2b10a62b0
Gorlomir <151527759+Gorlomir@users.noreply.github.com>
2024-05-22 21:15:55 +0500

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,8 @@
+name = 'Вячеслав'
+print('Имя:', name)
+age = 22
+print("Возраст:",age)
+New_age = (age + 1)
+print('Новый возраст:', New_age)
+is_student = (age <= 30)
+print( 'Студент:',is_student )
